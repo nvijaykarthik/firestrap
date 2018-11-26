b919c997113249fff51a99b6e12f3cb653ed6de6
vijaykarthik <nvijaykarthik@gmail.com>
2018-11-26 18:58:09 +0530

    UI change to make it working

--- AngularUI/app.js
+++ AngularUI/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const Eureka = require('eureka-js-client').Eureka;
+//const Eureka = require('eureka-js-client').Eureka;
 
 var indexRouter = require('./server/routes/index');
 var usersRouter = require('./server/routes/users');
@@ -18,13 +18,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'bin')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/server', indexRouter);
 app.use('/users', usersRouter);
  
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'bin/index.html'));
+  res.sendFile(path.join(__dirname, 'build/index.html'));
 });
 
 // catch 404 and forward to error handler
@@ -43,27 +43,27 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const client = new Eureka({
-  // application instance information
-  instance: {
-    app: 'UIService',
-    hostName: 'localhost',
-    ipAddr: '127.0.0.1',
-    port: 3000,
-    vipAddress: 'jq.test.something.com',
-	statusPageUrl: 'http://localhost:3000/info',
-    dataCenterInfo: {
-      name: 'MyOwn',
-    },
-  },
-  eureka: {
-    // eureka server host / port
-    host: 'localhost',
-    port: 8761,
-	servicePath: '/eureka/apps/'
-  },
-});
+// const client = new Eureka({
+//   // application instance information
+//   instance: {
+//     app: 'UIService',
+//     hostName: 'localhost',
+//     ipAddr: '127.0.0.1',
+//     port: 3000,
+//     vipAddress: 'jq.test.something.com',
+// 	statusPageUrl: 'http://localhost:3000/info',
+//     dataCenterInfo: {
+//       name: 'MyOwn',
+//     },
+//   },
+//   eureka: {
+//     // eureka server host / port
+//     host: 'localhost',
+//     port: 8761,
+// 	servicePath: '/eureka/apps/'
+//   },
+// });
 
-client.start();
+// client.start();
 
 module.exports = app;
