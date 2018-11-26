6d7e11967d7511299e811d3baf32eb5ddb73219a
vijaykarthik <nvijaykarthik@gmail.com>
2018-11-26 21:45:10 +0530

    Adding the angular project in Java runtime to manage it via eureka

---
+++ AngularUI/src/main/java/io/firestrap/AngularUI/AngularUiApplication.java
@@ -0,0 +1,12 @@
+package io.firestrap.AngularUI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularUiApplication.class, args);
+	}
+}
