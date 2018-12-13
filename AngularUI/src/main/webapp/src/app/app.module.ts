255c614e853d36aae739587614c84a4524148aa1
jrkrish82 <jrkrish82@github.com>
2018-12-13 16:32:00 +0530

    Angular UI initial framework for Firestrap home page with nav routing

--- AngularUI/src/main/webapp/src/app/app.module.ts
+++ AngularUI/src/main/webapp/src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    AppNavComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
+     BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
