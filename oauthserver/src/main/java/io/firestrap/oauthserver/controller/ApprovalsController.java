af27ba9ef234fe662c4c6c263144464ebe3a1f35
605420 <605420@PC369164.cts.com>
2019-01-28 17:47:40 +0530

    Oauth Enhancement changes

--- oauthserver/src/main/java/io/firestrap/oauthserver/controller/ApprovalsController.java
+++ oauthserver/src/main/java/io/firestrap/oauthserver/controller/ApprovalsController.java
@@ -1,12 +1,18 @@
 package io.firestrap.oauthserver.controller;
 
 
+import java.security.Principal;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-import org.springframework.ui.Model;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.provider.approval.Approval;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import io.firestrap.oauthserver.config.AuthorityPropertyEditor;
 import io.firestrap.oauthserver.config.SplitCollectionEditor;
@@ -27,6 +34,12 @@ public class ApprovalsController {
 	
 	
 	@Autowired
+	private JdbcClientDetailsService clientDetailsService;
+
+	@Autowired
+	private ApprovalStore approvalStore;
+	
+	@Autowired
 	ApprovalService approvalService;
 	
 	@InitBinder
@@ -37,9 +50,14 @@ public class ApprovalsController {
 	
 
 	@RequestMapping(value = "/getApprovals", method = RequestMethod.GET)	
-	public List<Approvals> getApprovals(Model model) {
-		List<Approvals> approvals = approvalService.getApprovals();				
-		return approvals;
+	public ModelAndView root(Map<String, Object> model, Principal principal) {
+
+		List<Approval> approvals = clientDetailsService.listClientDetails().stream()
+				.map(clientDetails -> approvalStore.getApprovals(principal.getName(), clientDetails.getClientId()))
+				.flatMap(Collection::stream).collect(Collectors.toList());
+		model.put("approvals", approvals);
+		return new ModelAndView("approval", model);
+
 	}
 	
 	
